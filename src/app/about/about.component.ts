a7038f7d859522817f8224664e89d59c050d461b
shrinivassab <shrinivassab@gmail.com>
2022-12-18 20:21:10 -0500

    Reactive Design

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import {fromEvent, interval, noop, Observable} from "rxjs";
+import {createHttpObservable} from "../common/util";
+import {map} from "rxjs/operators";
 
 @Component({
   selector: 'about',
@@ -11,26 +13,9 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    const http$ = Observable.create(observer => {
-      fetch('/api/courses')
-        .then(response => {
-          return response.json()
-        })
-        .then(body => {
-          observer.next(body);
-          observer.complete();
-        })
-        .catch(err => {
-          observer.error(err);
-        })
 
-    });
-
-    http$.subscribe(
-      courses => console.log(courses),
-      noop,
-      () => console.log('completed')
-    );
   }
 
 }
+
+
